6d61a83c5ae2799afd7451a93b6d8702531c729a
Dulio Denis <dulio.denis@yahoo.com>
2015-04-20 16:35:15 -0400

    Added the Game Interface Controller

---
+++ TitoSays/TitoSays WatchKit Extension/ViewControllers/TitoSaysGameInterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  TitoSaysGameInterfaceController.h
+//  TitoSays
+//
+//  Created by Dulio Denis on 4/20/15.
+//  Copyright (c) 2015 Dulio Denis. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface TitoSaysGameInterfaceController : WKInterfaceController
+
+@end
---
+++ TitoSays/TitoSays WatchKit Extension/ViewControllers/TitoSaysGameInterfaceController.m
@@ -0,0 +1,55 @@
+//
+//  TitoSaysGameInterfaceController.m
+//  TitoSays
+//
+//  Created by Dulio Denis on 4/20/15.
+//  Copyright (c) 2015 Dulio Denis. All rights reserved.
+//
+
+#import "TitoSaysGameInterfaceController.h"
+
+@interface TitoSaysGameInterfaceController ()
+@property (weak, nonatomic) IBOutlet WKInterfaceButton *upperLeftButton;
+@property (weak, nonatomic) IBOutlet WKInterfaceButton *upperRightButton;
+@property (weak, nonatomic) IBOutlet WKInterfaceButton *lowerLeftButton;
+@property (weak, nonatomic) IBOutlet WKInterfaceButton *lowerRightButton;
+@property (weak, nonatomic) IBOutlet WKInterfaceLabel *notificationLabel;
+@end
+
+@implementation TitoSaysGameInterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+    
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+
+#pragma mark - Button Action Methods
+
+- (IBAction)upperLeftTapped {
+}
+
+- (IBAction)upperRightTapped {
+}
+
+- (IBAction)lowerLeftTapped {
+}
+
+- (IBAction)lowerRightTapped {
+}
+
+@end
+
+
+
