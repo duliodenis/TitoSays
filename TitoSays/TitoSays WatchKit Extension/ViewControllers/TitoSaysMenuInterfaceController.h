55777fb96a4342d238d03286024f5bb14cc72321
Dulio Denis <dulio.denis@yahoo.com>
2015-04-20 16:10:05 -0400

    Added a Watch Extension Menu Interface Controller

---
+++ TitoSays/TitoSays WatchKit Extension/ViewControllers/TitoSaysMenuInterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  TitoSaysMenuInterfaceController.h
+//  TitoSays
+//
+//  Created by Dulio Denis on 4/20/15.
+//  Copyright (c) 2015 Dulio Denis. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface TitoSaysMenuInterfaceController : WKInterfaceController
+
+@end
---
+++ TitoSays/TitoSays WatchKit Extension/ViewControllers/TitoSaysMenuInterfaceController.m
@@ -0,0 +1,42 @@
+//
+//  TitoSaysMenuInterfaceController.m
+//  TitoSays
+//
+//  Created by Dulio Denis on 4/20/15.
+//  Copyright (c) 2015 Dulio Denis. All rights reserved.
+//
+
+#import "TitoSaysMenuInterfaceController.h"
+
+@interface TitoSaysMenuInterfaceController ()
+@property (weak, nonatomic) IBOutlet WKInterfaceButton *startGameButton;
+@property (weak, nonatomic) IBOutlet WKInterfaceLabel *gameOverLabel;
+@property (weak, nonatomic) IBOutlet WKInterfaceLabel *scoreLabel;
+@end
+
+@implementation TitoSaysMenuInterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+    
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+- (IBAction)startGame {
+    
+}
+
+@end
+
+
+
